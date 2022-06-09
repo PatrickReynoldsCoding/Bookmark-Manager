aec7dd92c2a3f6478bca683e37855773b4ce1c0d
PatrickReynoldsCoding <patrickreynoldscoding@gmail.com>
2022-06-09 16:50:07 +0100

    failing to get the PG gem to work

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -1,15 +1,30 @@
 require 'pg'
 
+
+
+
 class Bookmark
   def self.all
-    connection = PG.connect(dbname: 'bookmark_manager')
+    connection = PG.connect("host=localhost:5432 user=paddydb password=654654 dbname=bookmark_manager")
     result = connection.exec('SELECT * FROM bookmarks')
     result.map { |bookmark| bookmark['url'] }
-
-    # [
-    #   "http://www.makersacademy.com",
-    #   "http://www.destroyallsoftware.com",
-    #   "http://www.google.com"
-    #  ]
   end
 end
+
+# class Bookmark
+#   def self.all
+#     connection = PG::Connection.new('postgresql://paddydb:654654@localhost:5432/bookmark_manager')
+#     result = connection.exec('SELECT * FROM bookmarks')
+#     # result.map { |bookmark| bookmark['url'] }
+
+
+#     p result
+#     # [
+#     #   "http://www.makersacademy.com",
+#     #   "http://www.destroyallsoftware.com",
+#     #   "http://www.google.com"
+#     #  ]
+#   end
+# end
+
+Bookmark.all
